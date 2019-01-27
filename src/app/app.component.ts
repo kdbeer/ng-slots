fe539a8565c7fa6a49279e3b61666e8cdabbb249
Kridsada Duangmala <kridsadacpe@gmail.com>
2019-01-28 06:38:00 +0700

    save

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,8 @@ import { Slots } from '.';
 import { selectAllSlots } from './game.selectors';
 import { RollRandom } from './game.actions';
 
+const rewardList = [{ A: 1, B: 1, C: 1, reward: 'Home' }];
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -16,13 +18,35 @@ export class AppComponent implements OnInit {
 
   slots$: Observable<Slots>;
 
+  rewardText = '';
+
   constructor(private store$: Store<AppState>) {}
 
   ngOnInit(): void {
     this.slots$ = this.store$.pipe(select(selectAllSlots));
+
+    this.store$.pipe(select(selectAllSlots)).subscribe(res => {
+      this.rewardText = this.checkReward(res);
+    });
   }
 
   rolled() {
     this.store$.dispatch(new RollRandom());
+  }
+
+  checkReward(m: Slots): string {
+    const rewards = rewardList
+      .map(val => {
+        if (val.A === m.A && val.B === m.B && val.C === val.C) {
+          return val.reward;
+        }
+      })
+      .filter(val => val !== undefined);
+
+    if (rewards.length > 0) {
+      return rewards[0];
+    }
+
+    return 'คุณไม่ได้รางวัล';
   }
 }
